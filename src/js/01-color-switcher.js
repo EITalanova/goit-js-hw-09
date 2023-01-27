f68be44ab18d7f13a25198bbd187ccf78f36ded4
EITalanova <elenabelichenko77@gmail.com>
2023-01-27 15:28:57 -0800

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let interval = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function getColorBody(evt) {
+    body.style.backgroundColor = getRandomHexColor();
+}
+
+function changeColor(evt) {
+    interval = setInterval(getColorBody, 1000);
+    startBtn.setAttribute("disabled", "true");
+    stopBtn.removeAttribute("disabled");
+};
+
+function stopChangeColor() {
+    clearInterval(interval);
+    startBtn.removeAttribute("disabled");
+    stopBtn.setAttribute("disabled", "true");
+};
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChangeColor);
