690f5cc54f00f89190d57e6ddb1b1bcdad9a4a2f
EITalanova <98708004+EITalanova@users.noreply.github.com>
2023-01-30 19:29:54 +0000

    Deploying to gh-pages from @ EITalanova/goit-js-hw-09@6c71808a63aa9c7ec425611578ceaf159fb8f5c8 🚀

---
+++ 01-color-switcher.4e5030f3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let n=null;function o(t){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.disabled=!0,t.addEventListener("click",(function(d){n=setInterval(o,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.4e5030f3.js.map
